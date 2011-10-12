1cc441637a87fc10c904270c40f564546480c5c4
Matthew Slight <matthew@slight.me>
2011-10-12 19:13:16 +0100

    set up rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterHacks::Application.config.secret_token = '09ac5cd5c16951b320fa92ed3d392f9a39ff08da086b2eeda70736a8c3cbeb20603e75fbba2fef5571839aec1ef30125fe4ab81b1f47bfe1c4d347ddc8646ab0'
